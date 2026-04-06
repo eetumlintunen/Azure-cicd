1d7425b377b5075a3f2a1e909263f953dbb00115
AxeHammer87 <eetulintunen1@kamk.fi>
2026-04-06 18:17:34 +0300

    automaatio testi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Tervetuloa Azure CI/CD -harjoitukseen!"
+    return "Testataan automaatio! T: Eetu"
 
 if __name__ == "__main__":
     app.run()
